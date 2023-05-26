71abe572e1010004e2a246c19f65ac0fe1e463ea
MerlinDerEchte <merlinraabe@yahoo.de>
2023-05-26 12:30:04 +0200

    github pages config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+let assetPrefix = ''
+let basePath = '/'
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/sms/`
+  basePath = `/sms`
+}
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['images.ctfassets.net'],
-  }
+  },
+  assetPrefix: assetPrefix,
+  basePath:  basePath,
 }
 
 module.exports = nextConfig
