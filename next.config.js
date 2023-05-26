4634e6c9c41bb5c7af507b7f1a53eeb6083377ae
MerlinDerEchte <merlinraabe@yahoo.de>
2023-05-26 20:57:46 +0200

    removed domain asset path

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-let assetPrefix = ''
-let basePath = '/'
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/sms/`
-  basePath = `/sms`
-}
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['images.ctfassets.net'],
-  },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  }
 }
 
 module.exports = nextConfig
