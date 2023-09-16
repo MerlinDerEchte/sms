0af32e3609f211a7ac53e0ffbd5701507fbd23eb
MerlinDerEchte <merlinraabe@yahoo.de>
2023-09-16 14:37:48 +0200

    bfv script integration

--- pages/home.tsx
+++ pages/home.tsx
@@ -5,8 +5,6 @@ import { TeamsSection } from "components/Section/Sections/TeamsSection";
 import { ClubEventSection } from "components/Section/Sections/ClubEventsSection";
 import { BlogPostSection } from "components/Section/Sections/BlogPostSection";
 import { ImpressionsSection } from "components/Section/Sections/ImpressionsSection";
-import Head from "next/head";
-import Script from "next/script";
 export interface IcreateHomeStyles {
   screenWidth: number;
   screenHeight: number;
@@ -16,12 +14,7 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <Head>
-        <Script
-          src="https://widget-prod.bfv.de/widget/widgetresource/widgetjs"
-          strategy="beforeInteractive" // You can adjust this strategy based on your needs
-        />
-      </Head>
+
       <IntroSection />
       <ImpressionsSection />
       <TeamsSection />
