42b4b191c52157ffdd96f7229babeabc0f637dca
MerlinDerEchte <merlinraabe@yahoo.de>
2023-12-06 19:04:17 +0100

    scrolling

--- utils/scrollUtil.ts
+++ utils/scrollUtil.ts
@@ -1,7 +1,7 @@
 export const scrollIntoView = (id: string, callback?:() => void) => {
     const el: HTMLElement | null = document.getElementById(id);
     if (el) {
-        el.scrollIntoView({ behavior: "smooth", block: "start"})
+        el.scrollIntoView({ block: "start"})
         if(callback){
             callback();
         };
